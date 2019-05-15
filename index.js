f840110b0f136bb07a5d47333c07eb35a906925f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-05-14 22:47:36 -0400

    Basic block placing tests
    Development of this was not source-controlled, so the code is as-is for now

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var mc = require('minecraft-protocol');
 var mineflayer = require('mineflayer');
 var vec3 = require('vec3');
 
+const Block=require("prismarine-block")("1.12");
+const Item=require("prismarine-item")("1.12");
+
 var ips = [];
 var listP = [];
 var int = 0;
@@ -29,6 +32,22 @@ console.log(bot.players);
 });
 
 bot.once('login', function() {
+	bot.creative.setInventorySlot(36, new Item(1, 1));
+	var referenceBlock = bot.blockAt(bot.entity.position.offset(2, 0, 2));
+	
+	bot.on('move', () => {
+		referenceBlock = bot.blockAt(bot.entity.position.offset(2, 0, 2));
+		try {
+			bot.placeBlock(referenceBlock, new vec3(0, 1, 0), (err) => {
+				if (err) {
+					bot.chat(err.message);
+				}
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	});
+	bot.setControlState('right', true);
 });
 
 bot.on('end', function() {
