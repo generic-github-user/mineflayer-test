dfdb34a6378749bfb727c27ab30c4b9f4259b843
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-05-15 17:12:59 -0400

    Also disconnect from server before stopping program
    #4

--- command-execution.js
+++ command-execution.js
@@ -91,6 +91,7 @@ bot.once('login', function() {
 				} else if (split[1] == 'leave' || split[1] == 'quit' || split[1] == 'end') {
 					bot.chat('Bye!');
 					console.log('Disconnecting from server');
+					bot.quit();
 					process.exit();
 				}
 			}
